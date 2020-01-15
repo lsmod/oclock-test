96c056aa6475b7f8599220baa7dcb43185d85505
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-15 17:37:04 +0000

    Deploy lsmod/oclock-test to github.com/lsmod/oclock-test.git:gh-pages

---
+++ precache-manifest.5416090b4e057503748323392d47d2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2e858b2caef8c335b08fef208b1b84",
+    "url": "/oclock-test/index.html"
+  },
+  {
+    "revision": "d36762a21b6d50f2a2f5",
+    "url": "/oclock-test/static/css/main.cb84aa2c.chunk.css"
+  },
+  {
+    "revision": "3dab04cbbebf0207bd86",
+    "url": "/oclock-test/static/js/2.60a4a927.chunk.js"
+  },
+  {
+    "revision": "49309882d0acd110106c7be3de4100a8",
+    "url": "/oclock-test/static/js/2.60a4a927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d36762a21b6d50f2a2f5",
+    "url": "/oclock-test/static/js/main.e4a5aa9c.chunk.js"
+  },
+  {
+    "revision": "3d37a35001c19d5baba3",
+    "url": "/oclock-test/static/js/runtime-main.7f95062d.js"
+  },
+  {
+    "revision": "2361bbdd54814bd54c96c21d1c2165a1",
+    "url": "/oclock-test/static/media/denys-nevozhai-j8Bc4UerZXY-unsplash-reduced.2361bbdd.jpg"
+  }
+]);
